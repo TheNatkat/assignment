604361c5e0cc91bc5c6d47d339047bcc6d61dc7f
Akshit Singhal <iamak.bizwork@gmail.com>
2024-02-25 04:12:25 +0530

    first page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,392 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import React, { useEffect } from 'react'
+import { FaFacebookF, FaTwitter } from 'react-icons/fa'
+import { IoLogoInstagram } from 'react-icons/io'
+import { FaLinkedinIn } from 'react-icons/fa6'
+import { IoIosSearch } from 'react-icons/io'
+import jsonData from '../../public/oyebusy-dummy-data.json'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
-export default function Home() {
+const Page = () => {
+  useEffect(() => {
+    jsonData.sort((a, b) => Math.random() - 0.5)
+    console.log(jsonData)
+  }, [])
+
+  const handleClick = () => {
+    console.log(jsonData)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="home">
+      <div className="logo-container">
+        <img src={jsonData[0].meta['og:image']} className="logo-image" />
+      </div>
+      <div className="navbar-mobile">
+        <>
+          <img src={jsonData[0].meta['og:image']} className="logo-image" />
+        </>
+        <div className="burger">
+          <IoIosSearch />
+          <RxHamburgerMenu />
+        </div>
+      </div>
+      <div className={`navbar`}>
+        <div className="Social-media">
+          <FaFacebookF />
+          <IoLogoInstagram />
+          <FaLinkedinIn />
+          <FaTwitter />
+        </div>
+
+        <div className="navbar-mid">
+          <h6 className="page-selected" onClick={handleClick}>
+            Home
+          </h6>
+          <h6>Fashion</h6>
+          <h6>Technology</h6>
+          <h6>Life Style</h6>
+          <h6>Environment</h6>
+        </div>
+
+        <div className="search-bar">
+          <IoIosSearch />
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="main-section">
+        <div className="heading-container">
+          <div className="heading-single-box">
+            <img className="img-fill" src={jsonData[0].pageData.photo.cover} />
+
+            <div className="img-data">
+              <span className="img-data-category">{jsonData[0].category}</span>
+              <h3 className="img-data-heading">{jsonData[0].pageData.title}</h3>
+            </div>
+          </div>
+          <div className="heading-double-box">
+            <div className="first-box">
+              <img
+                className="img-fill"
+                src={jsonData[1].pageData.photo.cover}
+              />
+
+              <div className="img-data">
+                <span className="img-data-category">
+                  {jsonData[1].category}
+                </span>
+                <h3 className="img-data-heading">
+                  {jsonData[1].pageData.title}
+                </h3>
+              </div>
+            </div>
+            <div className="second-box">
+              <img
+                className="img-fill"
+                src={jsonData[2].pageData.photo.cover}
+              />
+
+              <div className="img-data">
+                <span className="img-data-category">
+                  {jsonData[2].category}
+                </span>
+                <h3 className="img-data-heading">
+                  {jsonData[2].pageData.title}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mid-heading-container">
+          <div className="hot-topic-container">
+            <div className="sub-heading">
+              <h6>Hot Topic</h6>
+              <hr />
+            </div>
+            <div className="hot-topic-content">
+              <div className="hot-topic-img-content">
+                <img
+                  className="img-fill"
+                  src={jsonData[2].pageData.photo.cover}
+                />
+
+                <div className="img-data">
+                  <span className="img-data-category">
+                    {jsonData[2].category}
+                  </span>
+                  <h3 className="img-data-heading">
+                    {jsonData[2].pageData.title}
+                  </h3>
+                </div>
+              </div>
+              <div className="hot-topic-just-content">
+                <div className="hot-topic-heading-container">
+                  <h3 className="img-data-heading">
+                    {jsonData[3].pageData.title}
+                  </h3>
+                  <h3 className="img-data-date">
+                    {jsonData[3].publish.slice(0, 10)}
+                  </h3>
+                </div>
+                <div className="hot-topic-heading-container">
+                  <h3 className="img-data-heading">
+                    {jsonData[4].pageData.title}
+                  </h3>
+                  <h3 className="img-data-date">
+                    {jsonData[4].publish.slice(0, 10)}
+                  </h3>
+                </div>
+                <div className="hot-topic-heading-container">
+                  <h3 className="img-data-heading">
+                    {jsonData[5].pageData.title}
+                  </h3>
+                  <h3 className="img-data-date">
+                    {jsonData[5].publish.slice(0, 10)}
+                  </h3>
+                </div>
+                <div className="hot-topic-heading-container">
+                  <h3 className="img-data-heading">
+                    {jsonData[6].pageData.title}
+                  </h3>
+                  <h3 className="img-data-date">
+                    {jsonData[6].publish.slice(0, 10)}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="custom-category-container">
+            <div className="sub-heading">
+              <h6>{jsonData[5].category}</h6>
+              <hr />
+            </div>
+            <div className="custom-category-section">
+              <div className="custom-category-img">
+                <img
+                  className="img-fill"
+                  src={jsonData[2].pageData.photo.cover}
+                />
+
+                <div className="img-data">
+                  <h3 className="img-data-heading">
+                    {jsonData[2].pageData.title}
+                  </h3>
+                </div>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-small"
+                  src={jsonData[2].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading">
+                  {jsonData[2].pageData.title}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="image-heading-container">
+          <div className="sub-heading">
+            <h6>Hot Topic</h6>
+            <hr />
+          </div>
+          <div className="image-heading-content">
+            <div className="img-container-medium">
+              <img
+                className="img-fill"
+                src={jsonData[0].pageData.photo.cover}
+              />
+              <div className="img-data">
+                <span className="img-data-category">
+                  {jsonData[0].category}
+                </span>
+                <h3 className="img-heading">{jsonData[0].pageData.title}</h3>
+              </div>
+            </div>
+            <div className="img-container-medium">
+              <img
+                className="img-fill"
+                src={jsonData[8].pageData.photo.cover}
+              />
+              <div className="img-data">
+                <span className="img-data-category">
+                  {jsonData[8].category}
+                </span>
+                <h3 className="img-heading">{jsonData[8].pageData.title}</h3>
+              </div>
+            </div>
+            <div className="img-container-medium">
+              <img
+                className="img-fill"
+                src={jsonData[11].pageData.photo.cover}
+              />
+              <div className="img-data">
+                <span className="img-data-category">
+                  {jsonData[11].category}
+                </span>
+                <h3 className="img-heading">{jsonData[11].pageData.title}</h3>
+              </div>
+            </div>
+            <div className="img-container-medium">
+              <img
+                className="img-fill"
+                src={jsonData[4].pageData.photo.cover}
+              />
+              <div className="img-data">
+                <span className="img-data-category">
+                  {jsonData[4].category}
+                </span>
+                <h3 className="img-heading">{jsonData[4].pageData.title}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="vertical-container-section">
+          <div className="vertical">
+            <div className="sub-heading">
+              <h6>Hot Topic</h6>
+              <hr />
+            </div>
+            <div className="vertical-data">
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[2].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[2].pageData.title}
+                </h3>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[6].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[6].pageData.title}
+                </h3>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[3].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[3].pageData.title}
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="vertical">
+            <div className="sub-heading">
+              <h6>Hot Topic</h6>
+              <hr />
+            </div>
+            <div className="vertical-data">
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[2].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[2].pageData.title}
+                </h3>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[6].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[6].pageData.title}
+                </h3>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[3].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[3].pageData.title}
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="vertical">
+            <div className="sub-heading">
+              <h6>Hot Topic</h6>
+              <hr />
+            </div>
+            <div className="vertical-data">
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[2].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[2].pageData.title}
+                </h3>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[6].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[6].pageData.title}
+                </h3>
+              </div>
+              <div className="custom-category-bottom">
+                <img
+                  className="img-xsmall"
+                  src={jsonData[3].pageData.photo.cover}
+                />
+
+                <h3 className="img-data-heading small-text">
+                  {jsonData[3].pageData.title}
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="Footer">
+        <div className="footer-heading">Magazine</div>
+        <div className="footer-subheading">A Magazine Theme</div>
+        <div className="footer-navbar">
+          <h6 className="page-selected-footer" onClick={handleClick}>
+            Home
+          </h6>
+          <h6>Fashion</h6>
+          <h6>Technology</h6>
+          <h6>Life Style</h6>
+          <h6>Environment</h6>
+        </div>
+        <div className="footer-social">
+          <FaFacebookF />
+          <IoLogoInstagram />
+          <FaLinkedinIn />
+          <FaTwitter />
+        </div>
+        <h5 className="copyright">
+          Copyright © 2023 Magazine - Powered by Rishi Theme
+        </h5>
       </div>
-    </main>
-  );
+    </div>
+  )
 }
+
+export default Page
